68f161afca14daf255cd80d986566ddfe9828cf5
Avi <avi@Avis-MacBook-Pro.local>
2023-08-16 20:35:39 -0600

    testing

--- client/src/config/axiosConfig.ts
+++ client/src/config/axiosConfig.ts
@@ -3,9 +3,9 @@ import axios, { AxiosInstance } from "axios";
 let baseURL = "https://houstable-backend.onrender.com";
 
 
-if (window.location.hostname === "localhost") {
-  baseURL = "http://localhost:3000"
-}
+// if (window.location.hostname === "localhost") {
+//   baseURL = "http://localhost:3000"
+// }
 
 const instance: AxiosInstance = axios.create({
   baseURL: baseURL,
