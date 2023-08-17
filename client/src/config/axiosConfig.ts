9fb401a447aff7be54d69a91f84e737c46095974
Avi <avi@Avis-MacBook-Pro.local>
2023-08-16 19:51:18 -0600

    change backend url

--- client/src/config/axiosConfig.ts
+++ client/src/config/axiosConfig.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 
 const instance: AxiosInstance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://houstable-backend.onrender.com",
 });
 
 export default instance;
